f2e096a860e27a74efea2de5d909f4e2a73da7a2
TpaeFawzen <30316430+GH-TpaeFawzen@users.noreply.github.com>
2020-12-06 22:11:07 +0900

    webhits.sh: wtf?
    Seemed code nonsense

--- webhits.sh
+++ webhits.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 #
 # webhits -- Reports amount of website hits, total values and per user hits by parsing the Apache Access log
 # Author: Prasanjit 
@@ -7,18 +7,15 @@
 # Declare the apache access log path by either using 
 WEBLOG="/var/log/httpd/access_log"
 
-if [ "$1" = "" ]; then
+case "$1" in ("")
  echo "Usage: `basename $0` <user1> <user2> osv.."
  exit
-fi
+esac
 date > wtstat
 echo "`cat $WEBLOG|wc -l` total hits on `hostname` " >>wtstat
-function_treff() {
- echo "`grep $i $WEBLOG|wc -l` hits on $i" >>wtstat
-}
 for i in $* 
 do
- function_treff
+ echo "`grep $i $WEBLOG|wc -l` hits on $i" >>wtstat
 done 
 sort -r wtstat -o wtstat
 more wtstat
